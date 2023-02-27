475e1e0cf682567ffdc9113d9f0044685a3d53db
alexander.vodianov <aleksandr.vodianov@protonmail.com>
2023-02-27 12:57:07 +0300

    update tests

--- tests/lua_install_test.sh
+++ tests/lua_install_test.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+wd=$(pwd)
+
+function _exit()
+{
+	rm -r $wd/build
+}
+
+trap _exit EXIT
 mkdir build && cd build
-	../../lua_install.sh
-cd .. && rm -r build
+lua_install.sh
+lua-*/src/lua -v 
