69ed7439991c15e79191c89e7b7fe29003c723d9
vodianov <126352902+vodianov@users.noreply.github.com>
2025-11-05 20:10:45 +0300

    add examples in python (#20)

---
+++ pyjamas/examples/lists_example.py
@@ -0,0 +1,15 @@
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print("First element:", my_list[0])
+print("Last element:", my_list[-1])
+
+# Modifying elements
+my_list[2] = 10
+print("Modified list:", my_list)
+
+# Appending and removing elements
+my_list.append(6)
+my_list.remove(4)
+print("Updated list:", my_list)
---
+++ pyjamas/examples/sets_example.py
@@ -0,0 +1,7 @@
+# Creating a set
+my_set = {1, 2, 3, 4, 4, 4}
+
+# Adding and removing elements
+my_set.add(5)
+my_set.remove(2)
+print("Updated set:", my_set)
